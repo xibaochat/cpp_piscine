4b1bdfb6b5fde72d4d3e9000a74457eadeb774fe
xibaochat <wmmxxbb@gmail.com>
2021-06-17 01:08:09 +0200

    add main message

--- c05/ex00/main.cpp
+++ c05/ex00/main.cpp
@@ -6,9 +6,9 @@ int main()
     {
         Bureaucrat  bureaucrat("Sesame", 5);
 
-        std::cout << bureaucrat << std::endl;
+        std::cout << bureaucrat << std::endl;// will affich
 		bureaucrat.increment();
-        std::cout << bureaucrat << std::endl;
+        std::cout << bureaucrat << std::endl;// will affich
     }
     catch(std::exception const &e)
     {
@@ -18,9 +18,9 @@ int main()
     try
     {
      	Bureaucrat  bureaucrat1("Sesame_mam", 1);
-		std::cout << bureaucrat1 << std::endl;
+		std::cout << bureaucrat1 << std::endl;//will
 		bureaucrat1.increment();
-		std::cout << bureaucrat1 << std::endl;
+		std::cout << bureaucrat1 << std::endl;// show err
     }
     catch(std::exception const &e)
     {
@@ -30,11 +30,11 @@ int main()
     {
      	Bureaucrat  bureaucrat1("Sesame_papa", 149);
 
-		std::cout << bureaucrat1 << std::endl;
+		std::cout << bureaucrat1 << std::endl;// show
 		bureaucrat1.decrement();
-		std::cout << bureaucrat1 << std::endl;
+		std::cout << bureaucrat1 << std::endl;//show
 		bureaucrat1.decrement();
-		std::cout << bureaucrat1 << std::endl;
+		std::cout << bureaucrat1 << std::endl;//err
 
     }
 	catch(std::exception const &e)
