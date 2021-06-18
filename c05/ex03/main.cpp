4092a54fce7c76b63e1fec96035fa1596ba2995b
xibaochat <wmmxxbb@gmail.com>
2021-06-19 01:31:27 +0200

    add ex04

--- c05/ex03/main.cpp
+++ c05/ex03/main.cpp
@@ -7,10 +7,31 @@
 
 int main()
 {
-	srand (time(NULL));
-
+	Bureaucrat bb("bb", 1);
 	Intern someRandomIntern;
 	Form *rrf;
 	rrf = someRandomIntern.makeForm("robotomy request", "Bender");
-	std::cout << *rrf << std::endl;
+	bb.signForm(*rrf);
+	bb.executeForm(*rrf);
+	Form *ss;
+	ss = someRandomIntern.makeForm("shrubbery creation", "petit vert");
+	bb.signForm(*ss);
+	bb.executeForm(*ss);
+
+	Form *pp;
+	pp = someRandomIntern.makeForm("presidential pardon", "boss");
+	bb.signForm(*pp);
+	bb.executeForm(*pp);
+
+	Form *fail_test;
+	fail_test = someRandomIntern.makeForm("wrong function name", "boss");
+	if (fail_test)
+	{
+		bb.signForm(*fail_test);
+		bb.executeForm(*fail_test);
+	}
+	delete rrf;
+	delete ss;
+	delete pp;
+	return 0;
 }
