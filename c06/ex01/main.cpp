b898f995f62336454bd8ce6a59e52b52d49b8b86
xibaochat <wmmxxbb@gmail.com>
2021-06-27 14:44:02 +0200

    ex01

--- c06/ex01/main.cpp
+++ c06/ex01/main.cpp
@@ -1,33 +1,27 @@
 #include <iostream>
 #include <stdint.h>
+#include "data.hpp"
 
-struct Data
-{
-  int a;
-  float b;
-   std::string s;
-} ;
-
+Data::Data(char const c, int const a, float const f, std::string const s):c(c), nb_int(a), nb_float(f), s(s)
+{}
 
 uintptr_t serialize(Data* ptr)
 {
-	void *p = ptr;
-	return uintptr_t(p);
+	return reinterpret_cast<uintptr_t>(ptr);
 }
 
 Data* deserialize(uintptr_t raw)
 {
-	void * p = (void *)raw;
-	return static_cast<Data *>(p);
+	return reinterpret_cast<Data *>(raw);
 }
 
 int main()
 {
-	Data data;
-	data.a = 42;
-	data.b = 42.0f;
-	data.s = "xibao";
-	std::cout << "ADDRESS :" << &data << std::endl;
-	std::cout << "serialize :" << serialize(&data) << std::endl;
-	std::cout << "decode " << deserialize(serialize(&data)) << std::endl;
+	Data *obj = new Data(65, 42, 42.024f, "conversion");
+	std::cout << "ADDRESS :" << obj << std::endl;
+	Data* decode = deserialize(serialize(obj));
+	std::cout << "Deserialize :" << decode << std::endl;
+	std::cout << "Deserialize " << "char c: " << decode->c << " int:" << decode->nb_int << " float:" << decode->nb_float << " string:" << decode->s << std::endl;
+
+	delete obj;
 }
